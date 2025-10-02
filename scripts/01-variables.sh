eabb976fc32eb738e1f77ffb505d8896325f9de1
Pedro Reis <164410222+pxxmdr@users.noreply.github.com>
2025-10-01 21:48:39 -0300

    Update 01-variables.sh

--- scripts/01-variables.sh
+++ scripts/01-variables.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+# >>>>> EDITE APENAS A SENHA <<<<<
+export LOCATION="brazilsouth"
+export RG="rg-safeyard"
+
+export RAND="$(openssl rand -hex 2)"
+
+export SQL_SERVER="sql-safeyard-$RAND"
+export SQL_ADMIN_USER="sqladmin"
+export SQL_ADMIN_PASS="PLACEHOLDER#TroqueAqui"   # <<<<< troque no Cloud Shell
+export SQL_DB="db-safeyard"
+
+export PLAN="plan-safeyard"
+export WEBAPP="app-safeyard-$RAND"
+
+echo "Variáveis definidas:"
+echo "RG=$RG | LOCATION=$LOCATION | RAND=$RAND"
+echo "SQL_SERVER=$SQL_SERVER | SQL_DB=$SQL_DB"
+echo "WEBAPP=$WEBAPP | PLAN=$PLAN"
