f175b1f17694aa5174a422a4f6b055d324566316
Pedro Reis <164410222+pxxmdr@users.noreply.github.com>
2025-10-01 21:50:32 -0300

    Update 04-create-appservice.sh

--- scripts/04-create-appservice.sh
+++ scripts/04-create-appservice.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+az appservice plan create \
+  -g "$RG" -n "$PLAN" --sku B1 --is-linux
+
+az webapp create \
+  -g "$RG" -p "$PLAN" \
+  -n "$WEBAPP" \
+  --runtime "JAVA:17-java17"
+
+az webapp show -g "$RG" -n "$WEBAPP" --query defaultHostName -o tsv
 
